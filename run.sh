5e1e5048dceadb9339861caccf0e0e69f1cc639c
Pushkar Kulkarni <pushkar.kulkarni@canonical.com>
2026-03-10 00:56:44 +0530

    modify run.sh

--- run.sh
+++ run.sh
@@ -4,20 +4,16 @@
 # openjdk-{17,21}-jdk
 # openjdk-{17,21}-fips-openssl-jre
 #
-# Test deps
-# junit - apt install junit5
 
-JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
-JAVA_FIPS_JRE=/usr/lib/jvm/java-17-openjdk-fips-openssl-amd64/
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64/
+JAVA_FIPS_JRE=/usr/lib/jvm/java-21-openjdk-fips-openssl-arm64/
 
-$JAVA_HOME/bin/javac *.java
+$JAVA_HOME/bin/javac -cp /usr/share/java/openssl-fips-java.jar *.java
 
 $JAVA_FIPS_JRE/bin/java CipherTest
 $JAVA_FIPS_JRE/bin/java MacTest
-$JAVA_FIPS_JRE/bin/java MessageDigestTest
+$JAVA_FIPS_JRE/bin/java MDTest
 $JAVA_FIPS_JRE/bin/java KeyAgreementTest
-$JAVA_FIPS_JRE/bin/java KeyFactoryTest
-$JAVA_FIPS_JRE/bin/java KeyPairGeneratorTest
-$JAVA_FIPS_JRE/bin/java KeyStoreTest
 $JAVA_FIPS_JRE/bin/java SecretKeyFactoryTest
 $JAVA_FIPS_JRE/bin/java SecureRandomTest
+$JAVA_FIPS_JRE/bin/java SignatureTest
